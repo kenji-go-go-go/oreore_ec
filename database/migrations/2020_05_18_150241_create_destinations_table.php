bd9104d39e44f5c59cb77bc3a7e58003fc085b66
kenji-go-go-go <FZF01113@nifty.com>
2020-06-20 21:43:07 +0900

    CreateDestinationsTableの修正

--- database/migrations/2020_05_18_150241_create_destinations_table.php
+++ database/migrations/2020_05_18_150241_create_destinations_table.php
@@ -15,17 +15,19 @@ class CreateDestinationsTable extends Migration
     {
         Schema::create('destinations', function (Blueprint $table) {
             $table->id();
-            $table->timestamp('created_at');
-            $table->timestamp('updated_at')->nullable();
-            $table->timestamp('deleted_at')->nullable();
-            $table->string('user_id');
+            $table->timestamps();
+            $table->softDeletes('deleted_at');
+            /**
+            *外部キー制約が必要になる？
+            **/
+            $table->unsignedBigInteger('user_id');
             $table->string('name');
             $table->string('tel');
             $table->string('zipcode');
             $table->string('prefecture');
             $table->string('city');
-            $table->string('address1');
-            $table->string('address2');
+            $table->text('address1');
+            $table->text('address2');
         });
     }
 
